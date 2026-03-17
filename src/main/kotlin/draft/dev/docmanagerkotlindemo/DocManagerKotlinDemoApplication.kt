39e1c3978d41ffd477eb0864975c20837961db99
Your Name <your.email@example.com>
2026-03-17 12:56:58 +0200

    Initial Commitment and Push

---
+++ src/main/kotlin/draft/dev/docmanagerkotlindemo/DocManagerKotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package draft.dev.docmanagerkotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DocManagerKotlinDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DocManagerKotlinDemoApplication>(*args)
+}
---
+++ src/test/kotlin/draft/dev/docmanagerkotlindemo/DocManagerKotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package draft.dev.docmanagerkotlindemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DocManagerKotlinDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
